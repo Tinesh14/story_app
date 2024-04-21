51713c66bf45f37eb526134e76a5f6ef55254c71
Tinesh Roy <tinesh12.roy@gmail.com>
2024-04-21 16:18:37 +0700

    tampilkan info window marker

--- lib/widget/maps_widget.dart
+++ lib/widget/maps_widget.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoding/geocoding.dart' as geo;
 
 class MapsScreen extends StatefulWidget {
   LatLng location;
@@ -34,10 +35,20 @@ class _MapsScreenState extends State<MapsScreen> {
             ),
             markers: markers,
             mapType: selectedMapType,
-            onMapCreated: (controller) {
+            onMapCreated: (controller) async {
+              final info = await geo.placemarkFromCoordinates(
+                  widget.location.latitude, widget.location.longitude);
+              final place = info[0];
+              final street = place.street!;
+              final address =
+                  '${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
               final marker = Marker(
-                markerId: const MarkerId("dicoding"),
+                markerId: const MarkerId("story_app"),
                 position: widget.location,
+                infoWindow: InfoWindow(
+                  title: street,
+                  snippet: address,
+                ),
                 onTap: () {
                   controller.animateCamera(
                     CameraUpdate.newLatLngZoom(widget.location, 18),
